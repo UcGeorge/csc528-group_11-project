4ad9f06d7f61f6fa3b333f0595b1d712f2d1130a
UcGeorge <petegeorge20005@gmail.com>
2023-05-02 21:24:46 +0100

    implemented traditional DFS

--- usm_implementation/methods/dfs.py
+++ usm_implementation/methods/dfs.py
@@ -8,51 +8,109 @@ if they have not already been visited.
 The search continues until the goal vertex is found or the stack is empty.
 """
 
-from usm_implementation.types import Vertex, Path, Graph
+from typing import Dict, Union, Set, List, Tuple
+
+# The various data types used in this module
+Vertex = Union[int, str]
+Graph = Dict[Vertex, Set[Vertex]]
+Path = List[Vertex]
+VertexPositionMap = Dict[Vertex, Tuple[float, float]]
+PathMap = Dict[Vertex, Union[Vertex, None]]
+
+# Graph data structure
+graph: Graph = {}
+
+
+def _compose_path(v: Vertex, paths: PathMap) -> Path:
+    """
+    Composes a path from the root node to a given vertex.
+
+    Args:
+    - v (Vertex): The vertex to start the path from.
+    - paths (PathMap): A map that maps vertices to their direct parents after a DFS is performed.
+
+    Returns:
+        Path: A list of vertices that represents the path from the root to the goal.
+    """
+    path: Path = [v]
+
+    while paths[v]:
+        path.insert(0, paths[v])
+        v = paths[v]
+
+    return path
+
 
 def perform_dfs(root: Vertex, goal: Vertex, graph: Graph) -> Path:
     """
-    Description:
     This function performs a depth-first search on a graph starting from a given root 
     and finds a path from the root to a given goal node. It returns a set of vertices in the path.
-    
-    Parameters:
-    root: A [Vertex] representing the starting vertex of the search.
-    goal: A [Vertex] representing the goal vertex to be reached.
 
-    Return Value:
-    A set of [Vertex] representing the path from the root to the goal vertex, if a path exists. 
-    If no path is found, an empty set is returned.
+    Args:
+    - root (Vertex): A Vertex representing the starting vertex of the search.
+    - goal (Vertex): A Vertex representing the goal vertex to be reached.
+    - graph (Graph): A Graph representing the graph search space represented as an adjacency list.
+
+    Returns:
+        A list of vertices representing the path from the root to the goal vertex, if a path exists. 
+        If no path is found, an empty set is returned.
     """
-    path = list({})
-    stack = list({root})
-    memory: Path = set({})
+    stack: List[Vertex] = list([root])
+    visited: Set[Vertex] = set()
+    paths: PathMap = {v: None for v in graph}
 
-    focus: Vertex = stack.pop(-1)
-    path.append(focus)
-    memory.add(focus)
+    while stack:
+        # Remove Vertex from the stack
+        vertex = stack.pop(-1)
 
-    # while stack:
-    #     vertex = stack.pop()
-    #     if vertex not in visited:
-    #         visited.add(vertex)
-    #         if vertex == goal:
-    #             return visited
-    #         stack.extend(graph[vertex] - visited)
+        # This Vertex has not been explored
+        if vertex not in visited:
+            # Add Vertex to the list of visited vertices
+            visited.add(vertex)
 
-    while focus and focus != goal:
-        # This Vertex has been explored
-        if focus in memory:
-            # Remove from the path
-            path.pop()
-            continue
+            # Vertex is the goal
+            if vertex == goal:
+                # Return the path to the vertex
+                return _compose_path(vertex, paths)
 
-        # This Vertex is a leaf node
-        if len(graph[focus]) == 0:
-            # Remove from the path
-            path.pop()
-            continue
+            # Add all adjacent vertices that have not been visited to stack
+            adjacent_vertices = graph[vertex] - visited - set(stack)
+            stack.extend(adjacent_vertices)
+
+            for v in adjacent_vertices:
+                paths[v] = vertex
+
+    return set()
 
 
+if __name__ == "__main__":
+    print("\n_-_-_-_-_-_-_-_-_-_-_-_-_-_RUNNING DFS USM SEARCH_-_-_-_-_-_-_-_-_-_-_-_-_-_\n")
 
-    pass
+    # Get a list of vertices from user
+    vertices: "list[str]" = list(map(
+        lambda e: e.strip(),
+        input("Input graph verticies: ").split(",")))
+
+    # Get root & goal vertices from user
+    root = input("Input root vertex: ")
+    goal = input("Input goal vertex: ")
+
+    print()
+
+    # Get the adjacency list for each vertex from user
+    for v in vertices:
+        graph[v] = set(map(
+            lambda e: e.strip(),
+            input(f"Input verticies connected to the vertex [{v}]: ").split(",")))
+
+    print("\n_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-\n")
+
+    try:
+        # Perform DFS and print the result
+        print(f"[PATH] {perform_dfs(root=root, goal=goal, graph=graph)}")
+    except KeyboardInterrupt:
+        print("[Execution interrupted by user]")
+    except:
+        print("[An unknown error occured]")
+
+    # print(f"[PATH] {perform_dfs(root=root, goal=goal, graph=graph)}")
--- usm_implementation/types.py
+++
@@ -1,9 +0,0 @@
-"""
-This module defines the various data types used throughout the software
-"""
-
-from typing import Dict, Union, Set
-
-Vertex = Union[int, str]
-Graph = Dict[Vertex, Set[Vertex]]
-Path = Set[Vertex]
